58607acd9fc3a788ca1991ff9c4aef447195b81a
Ashton Dunderdale <121163457+ashtondunderdale@users.noreply.github.com>
2023-11-08 14:29:13 +0000

    added popupmenu display

--- kanban/lib/main.dart
+++ kanban/lib/main.dart
@@ -17,16 +17,51 @@ class KanBanApplication extends StatelessWidget {
             ),
           ),
           actions: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(right: 1200), 
-              child: DropDown(),
+            Row(
+              children: [
+                DropDown(),
+                SizedBox(width: 16), 
+                Container(
+                  padding: EdgeInsets.only(right: 1100, left: 100), 
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 255, 255, 255),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 8),
+                  child: Text(
+                    'Sprint 16',
+                    style: TextStyle(
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(1),
+                  child: Text(
+                    '06/11/2023 -> 17/11/2023',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                      fontSize: 14,
+                    ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ],
         ),
         body: Column(
           children: [
             SizedBox(
-              height: 600, 
+              height: 750, 
               child: Dashboard(),
             ),
           ],
@@ -35,6 +70,7 @@ class KanBanApplication extends StatelessWidget {
     );
   }
 }
+
 
 class DropDown extends StatefulWidget {
   const DropDown({Key? key}) : super(key: key);
@@ -57,24 +93,76 @@ class _DropDownState extends State<DropDown> {
           height: 2,
           color: Color.fromARGB(255, 90, 90, 90),
         ),
+
         onChanged: (String? newValue) {
           setState(() {
             dropDownValue = newValue!;
           });
         },
-        items: const [
+
+        items: [
           DropdownMenuItem<String>(
             value: 'Default',
             child: Text('Default'),
           ),
+
           DropdownMenuItem<String>(
             value: 'All',
             child: Text('All'),
           ),
+          
           DropdownMenuItem<String>(
-            value: 'Specific',
-            child: Text('Specific'),
-          ),
+            child: PopupMenuButton<String>(
+              itemBuilder: (context) {
+                return <PopupMenuEntry<String>>[
+
+                  PopupMenuItem<String>(
+                    value: 'Peter Roden',
+                    child: Text('Peter Roden'),
+                  ),
+
+                  PopupMenuItem<String>(
+                    value: 'Susannah Marshall',
+                    child: Text('Susannah Marshall'),
+                  ),
+
+                  PopupMenuItem<String>(
+                    value: 'Josh Sweeney',
+                    child: Text('Josh Sweeney'),
+                  ),
+
+                  PopupMenuItem<String>(
+                    value: 'Bart Wojda',
+                    child: Text('Bart Wojda'),
+                  ),
+
+                  PopupMenuItem<String>(
+                    value: 'Ashton Dunderdale',
+                    child: Text('Ashton Dunderdale'),
+                  ),
+
+                  PopupMenuItem<String>(
+                    value: 'Harrison O\'Leary',
+                    child: Text('Harrison O\'Leary'),
+                  ),
+                  
+                  PopupMenuItem<String>(
+                    value: 'Charlie Glover',
+                    child: Text('Charlie Glover'),
+                  ),
+                ];
+              },
+
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Specific'),
+                  Icon(Icons.arrow_right_rounded),
+                ],
+              ),
+            ),
+          )
+
         ],
       ),
     );
@@ -93,8 +181,7 @@ class Dashboard extends StatelessWidget {
         children: [
           Container(
             margin: EdgeInsets.symmetric(horizontal: 8, vertical: 20),
-            width: 200, 
-            height: 700,
+            width: MediaQuery.of(context).size.width * 0.15,
             color: Color.fromARGB(255, 248, 248, 248),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(10),
@@ -102,144 +189,162 @@ class Dashboard extends StatelessWidget {
                 children: [
                   Container(
                     width: double.infinity,
-                    color: Color.fromARGB(255, 156, 210, 255), 
-                    child: Text(
-                      'Backlog',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.white, 
+                    color: Color.fromARGB(255, 106, 180, 240),
+                    height: 40,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Backlog',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
                 ],
               ),
-            )
+            ),
           ),
+
           Container(
             margin: EdgeInsets.symmetric(horizontal: 8, vertical: 20),
-            width: 200, 
-            height: 700,
+            width: MediaQuery.of(context).size.width * 0.15,
             color: Color.fromARGB(255, 248, 248, 248),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Column(
-              children: [
-                Container(
-                  width: double.infinity,
-                  color: Color.fromARGB(255, 106, 180, 240), 
-                  child: Text(
-                    'In Development',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.white, 
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    color: Color.fromARGB(255, 55, 142, 212),
+                    height: 40,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        'In Development',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          )
           ),
+
           Container(
             margin: EdgeInsets.symmetric(horizontal: 8, vertical: 20),
-            width: 200, 
-            height: 700,
+            width: MediaQuery.of(context).size.width * 0.15,
             color: Color.fromARGB(255, 248, 248, 248),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Column(
-              children: [
-                Container(
-                  width: double.infinity,
-                  color: Color.fromARGB(255, 28, 118, 192), 
-                  child: Text(
-                    'Developed',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.white, 
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    color: Color.fromARGB(255, 47, 119, 179),
+                    height: 40,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Developed',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          )
           ),
+
           Container(
             margin: EdgeInsets.symmetric(horizontal: 8, vertical: 20),
-            width: 200, 
-            height: 700,
+            width: MediaQuery.of(context).size.width * 0.15,
             color: Color.fromARGB(255, 248, 248, 248),
-          child: ClipRRect(           
-            borderRadius: BorderRadius.circular(10),
-            child: Column(
-              children: [
-                Container(
-                  width: double.infinity,
-                  color: Color.fromARGB(255, 103, 140, 170), 
-                  child: Text(
-                    'In Test',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.white, 
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    color: Color.fromARGB(255, 115, 156, 189),
+                    height: 40,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        'In Test',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          )
           ),
+
           Container(
             margin: EdgeInsets.symmetric(horizontal: 8, vertical: 20),
-            width: 200, 
-            height: 700,
+            width: MediaQuery.of(context).size.width * 0.15,
             color: Color.fromARGB(255, 248, 248, 248),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Column(
-              children: [
-                Container(
-                  width: double.infinity,
-                  color: Color.fromARGB(255, 19, 109, 184), 
-                  child: Text(
-                    'Tested',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.white, 
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    color: Color.fromARGB(255, 51, 90, 122),
+                    height: 40,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Tested',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          )
           ),
-          Container(
+
+                    Container(
             margin: EdgeInsets.symmetric(horizontal: 8, vertical: 20),
-            width: 200, 
-            height: 700,
+            width: MediaQuery.of(context).size.width * 0.15,
             color: Color.fromARGB(255, 248, 248, 248),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Column(
-              children: [
-                Container(
-                  width: double.infinity,
-                  color: Color.fromARGB(255, 137, 202, 255), 
-                  child: Text(
-                    'Done',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.white, 
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    color: Color.fromARGB(255, 13, 101, 173),
+                    height: 40,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Done',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          )
           ),
         ],
       ),
