49f892fa4dce6b1e9daf318a1f040331437bf4a2
Ashton Dunderdale <121163457+ashtondunderdale@users.noreply.github.com>
2023-12-07 19:13:51 +0000

    refactor

--- kanban/lib/main.dart
+++ kanban/lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
-
-import 'app.dart';
+import 'views/home_view.dart';
 
 void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "kanban",
+      home: HomeView(),
+    );
+  }
+}
