e132cb2ec89975fa7fed55738583a7c3cca2086d
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-04-16 22:14:27 +0100

    reset

--- kanban/lib/main.dart
+++ kanban/lib/main.dart
@@ -1,41 +1,15 @@
 import 'package:flutter/material.dart';
 
-import 'package:kanban_application/utils/theme/dark_theme.dart';
-import 'package:kanban_application/utils/theme/light_theme.dart';
-import 'package:kanban_application/utils/theme/theme.dart';
-import 'package:kanban_application/views/home.dart';
+void main() {
+  runApp(const App());
+}
 
 
-void main() => runApp(const App());
-
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-
-  @override
-  void initState() {
-    super.initState();
-    currentTheme.addListener(() {
-      setState(() {});
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "kanban",
-      home: HomeView(),
-      theme: lightTheme,
-      darkTheme: darkTheme.copyWith(
-        scrollbarTheme: ScrollbarThemeData(thumbColor: MaterialStatePropertyAll(Colors.white)),
-      ),
-      themeMode: currentTheme.currentTheme(),
-      debugShowCheckedModeBanner: false,
-    );
+    return const Placeholder();
   }
 }
